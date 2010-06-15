46fecd1a7bb634218889a1e93ff85b7085e7f777
Tim Wingfield <tim@timwingfield.com>
2010-06-14 23:30:42 -0400

    Added stubs for Bank Repo and Banker Service. No tests, just stubbed out the method names on the classes.

---
+++ src/Monopoly/BankRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    public interface IBankRepository
+    {
+        int CreditAccount(Player player, int amount);
+        int DebitAccount(Player player, int amount);
+        int CheckBalance(Player player);
+    }
+
+    public class BankRepository : IBankRepository
+    {
+        public int CreditAccount(Player player, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DebitAccount(Player player, int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CheckBalance(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Monopoly/BankerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopoly
+{
+    public interface IBankerService
+    {
+        void PassGo(Player player);
+        void BuyProperty(Player player, BaseProperty property);
+        void PayRent(Player player, int rentDue);
+    }
+
+    public class BankerService : IBankerService
+    {
+        public void PassGo(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuyProperty(Player player, BaseProperty property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PayRent(Player player, int rentDue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
