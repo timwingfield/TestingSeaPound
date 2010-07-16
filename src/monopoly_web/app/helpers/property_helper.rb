f1e4b08b268e2c9d299c9705956450c639472ed1
Tim Wingfield <tim@timwingfield.com>
2010-07-15 21:14:45 -0400

    removing rails app from Codestock from the master branch

--- src/monopoly_web/app/helpers/property_helper.rb
+++
@@ -1,2 +0,0 @@
-module PropertyHelper
-end
--- src/monopoly_web/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'ea99c3d6b893f7a2ed0848a1eb7cfe6b21dabeaa2d638a7542e083514df56af1dc553355383645c6bdef7561985b955a02de9ee3e71b0faabf29bce2a6a33b0c';
