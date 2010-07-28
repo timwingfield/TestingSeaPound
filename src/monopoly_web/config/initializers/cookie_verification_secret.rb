a2c3ac1cce0418c7329020f4ee8ca88a92307a21
Tim Wingfield <tim@timwingfield.com>
2010-07-28 08:45:26 -0400

    adding rails site from CINNUG presentation

---
+++ src/monopoly_web/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'e58d8316c619e799c01d4b847fe8d6c554bc2ac6091c5d32d7a4ae743632ba4263523cc0d1fdf2dc3f270a34b0e22cca30b3d7701138320d17903c72ed84c956';
